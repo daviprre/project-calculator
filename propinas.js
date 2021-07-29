01dd8e608feb9876684fe478ec99361bee9beb9f
Stiven Preciado <davidspreciado@gmail.com>
2021-07-28 22:12:49 -0500

    Primera ensayo con JS

---
+++ propinas.js
@@ -0,0 +1,18 @@
+var valorBoton = 0;
+var porcentaje = 0;
+
+
+function ObtenerPorcentaje5(){
+    valorBoton = document.getElementById("button5");
+    porcentaje = valorBoton.value;
+    console.log(porcentaje);
+
+}
+
+function ObtenerPorcentaje10(){
+    valorBoton = document.getElementById("button10");
+    porcentaje = valorBoton.value;
+    console.log(porcentaje);
+}
+
+
