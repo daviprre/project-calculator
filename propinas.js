1437746c9056864bd67dc8d9d0acaa4ca90e3623
Stiven Preciado <davidspreciado@gmail.com>
2021-07-29 23:03:23 -0500

    Primer implementacion del javascript

--- propinas.js
+++ propinas.js
@@ -1,17 +1,124 @@
 var valorBoton = 0;
 var porcentaje = 0;
-porcentaje = valorBoton.value;
+var cantidadPersonas = 0;
+var valorServicio= 0;
+var valorIndividual = document.getElementById("valorIndividual");
+var valorTotal = document.getElementById("valorTotal");
+var mensaje = document.getElementById("mensajeValidacion");
+var cantidad = document.getElementById("CantidadPersonas");
+var PrecioTotal = document.getElementById("valorServicio");
+valorIndividual.innerText = "$" + "0.00";
+valorTotal.innerText = "$" + "0.00";
+mensaje.innerText = "";
 
 
-function ObtenerPorcentaje(){
-    valorBoton = document.getElementById("button");
-    return valorBoton;
+
+function ObtenerPorcentaje5(){
+    valorBoton = document.getElementById("button5");
+    porcentaje = valorBoton.value;
+    cantidadPersonas = cantidad.value;
+    valorServicio = parseInt(PrecioTotal.value);
+
+        if(cantidadPersonas===0 || cantidadPersonas == ""){
+            mensaje.innerText = "Can't be zero";
+            mensaje.style="color: red; font-weight: 700";
+            cantidad.style="border: 2px solid red"
+        }
+        else{
+            var propinaIndividual = ((valorServicio * porcentaje) / 100) / cantidadPersonas;
+            var totalIndividual = (((valorServicio * porcentaje) / 100) + valorServicio) / cantidadPersonas;
+            valorIndividual.innerText = "$" + propinaIndividual;
+            valorTotal.innerText = "$" + totalIndividual;
+        }
 }
 
-/* function ObtenerPorcentaje10(){
+function ObtenerPorcentaje10(){
     valorBoton = document.getElementById("button10");
     porcentaje = valorBoton.value;
-    console.log(porcentaje);
-} */
+    cantidadPersonas = cantidad.value;
+    var PrecioTotal = document.getElementById("valorServicio");
+    valorServicio = parseInt(PrecioTotal.value);
 
+    if(cantidadPersonas===0 || cantidadPersonas == ""){
+        mensaje.innerText = "Can't be zero";
+        mensaje.style="color: red; font-weight: 700";
+        cantidad.style="border: 2px solid red"
+    }
+    else{
+        var propinaIndividual = ((valorServicio * porcentaje) / 100) / cantidadPersonas;
+        var totalIndividual = (((valorServicio * porcentaje) / 100) + valorServicio) / cantidadPersonas;
+        valorIndividual.innerText = "$" + propinaIndividual;
+        valorTotal.innerText = "$" + totalIndividual;
+    }
+}
+
+function ObtenerPorcentaje15(){
+    valorBoton = document.getElementById("button15");
+    porcentaje = valorBoton.value;
+    cantidadPersonas = cantidad.value;
+    var PrecioTotal = document.getElementById("valorServicio");
+    valorServicio = parseInt(PrecioTotal.value);
+  
+    if(cantidadPersonas===0 || cantidadPersonas == ""){
+        mensaje.innerText = "Can't be zero";
+        mensaje.style="color: red; font-weight: 700";
+        cantidad.style="border: 2px solid red"
+    }
+    else{
+        var propinaIndividual = ((valorServicio * porcentaje) / 100) / cantidadPersonas;
+        var totalIndividual = (((valorServicio * porcentaje) / 100) + valorServicio) / cantidadPersonas;
+        valorIndividual.innerText = "$" + propinaIndividual;
+        valorTotal.innerText = "$" + totalIndividual;
+    }
+}
+
+function ObtenerPorcentaje25(){
+    valorBoton = document.getElementById("button25");
+    porcentaje = valorBoton.value;
+    cantidadPersonas = cantidad.value;
+    var PrecioTotal = document.getElementById("valorServicio");
+    valorServicio = parseInt(PrecioTotal.value);
+   
+    if(cantidadPersonas===0 || cantidadPersonas == ""){
+        mensaje.innerText = "Can't be zero";
+        mensaje.style="color: red; font-weight: 700";
+        cantidad.style="border: 2px solid red"
+    }
+    else{
+        var propinaIndividual = ((valorServicio * porcentaje) / 100) / cantidadPersonas;
+        var totalIndividual = (((valorServicio * porcentaje) / 100) + valorServicio) / cantidadPersonas;
+        valorIndividual.innerText = "$" + propinaIndividual;
+        valorTotal.innerText = "$" + totalIndividual;
+    }
+}
+
+function ObtenerPorcentaje50(){
+    valorBoton = document.getElementById("button50");
+    porcentaje = valorBoton.value;
+    cantidadPersonas = cantidad.value;
+    var PrecioTotal = document.getElementById("valorServicio");
+    valorServicio = parseInt(PrecioTotal.value);
+    
+    if(cantidadPersonas===0 || cantidadPersonas == ""){
+        mensaje.innerText = "Can't be zero";
+        mensaje.style="color: red; font-weight: 700";
+        cantidad.style="border: 2px solid red"
+    }
+    else{
+        var propinaIndividual = ((valorServicio * porcentaje) / 100) / cantidadPersonas;
+        var totalIndividual = (((valorServicio * porcentaje) / 100) + valorServicio) / cantidadPersonas;
+        valorIndividual.innerText = "$" + propinaIndividual;
+        valorTotal.innerText = "$" + totalIndividual;
+    }
+}
+
+function BorrarCampos(){
+    cantidad.value=0;
+    cantidad.style="border:0"
+    mensaje.innerText = "";
+    PrecioTotal.value=0.00;
+    valorIndividual.innerText = "$" + "0.00";
+    valorTotal.innerText = "$" + "0.00";
+
+}
 
